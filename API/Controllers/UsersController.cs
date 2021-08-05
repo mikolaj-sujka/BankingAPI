cfd4ca69d11306e7030a9e0a948c70073a4892c5
Mikolaj Sujka <mikolajsujka@gmail.com>
2021-08-05 16:42:50 +0200

    Creating database etc

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿
+using API.AppInterfaces;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly IBankUserRepo _userRepo;
+
+        public UsersController(IBankUserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BankUser>>> GetUsers()
+        {
+            return await _userRepo.GetUsersAsync();
+        }
+
+        // api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BankUser>> GetUserById([FromRoute] int id)
+        {
+            var user = _userRepo.GetUserByIdAsync(id);
+            return await user;
+        }
+    }
+}
---
+++ API/Entities/BankUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class BankUser
+    {
+        // mail, name, lastname, postalcode, city, country, datebirth, pin, username (jest)
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        
+    }
+}
